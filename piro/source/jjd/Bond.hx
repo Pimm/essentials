1f7d210277138a690bdb2d437d3306733f4d46d6
jdonaldson <jdonaldson@gmail.com>
2011-03-25 09:46:53 -0400

    cleaned up typing to avoid casts

--- piro/source/jjd/Bond.hx
+++ piro/source/jjd/Bond.hx
@@ -61,23 +61,27 @@ class Bond2<A,B,C> implements IBond {
 	}
 }
 
-class Bond3<A,B,C,D> {
+class Bond3<A,B,C,D> implements IBond {
 	private var f:A->B->C->D;
 	private var a:Async<A>;	
 	private var b:Async<B>;
 	private var c:Async<C>;
 	private var d:Async<D>;
-	
+	private var _halt:Bool;
 	public function new( f:A->B->C->D, a:Async<A>, b:Async<B>, c:Async<C>, d:Async<D>){
 		this.f = f;
 		this.a = a;
 		this.b = b;
 		this.c = c;
-		this.d = d;		
+		this.d = d;	
+		_halt = false;	
 	}
 	
+	public function halt(){ _halt = true; }
+	public function resume(){ _halt = false; }
+	
 	public function update(){
-		if (!halt && Bond.allSet(cast [a,b,c])) d.yield(f(a.val, b.val, c.val));
+		if (!_halt && Bond.allSet(cast [a,b,c])) d.yield(f(a.val, b.val, c.val));
 	}
 }
 
