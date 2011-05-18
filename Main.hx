735380bf55c7be54e17805d670f6f50e4a7d00ad
Pimm Hogeling <pimmhogeling@gmail.com>
2011-05-18 23:07:23 +0200

    Changed the sandbox we use to try out Piro

--- Main.hx
+++ Main.hx
@@ -8,27 +8,42 @@ class Main {
 		// Create the random number array.
 		var numbers:Array<Int> = new Array();
 		for (number in 0...16) {
-			numbers.insert(Math.floor(number * Math.random()), number);
+			numbers.insert(Math.floor((1 + number) * Math.random()), number);
 		}
 		trace(numbers);
-		// Create the asyncronous value for the tracable method.
-		var message:Async<String> = new Async(Main);
+		// Wire the function that will turn the index into a message.
+		var targetIndex:Async<Int> = new Async(Main);
+		var message:Async<String> = function(value:Int):String {
+			return if (0 > value) {
+				if (-1 == value) {
+					"The number 7 was found 1 place from the end.";
+				} else {
+					"The number 7 was found " + -value + " places from the end.";
+				}
+			} else {
+				if (1 == value) {
+					"The number 7 was found 1 place from the start.";
+				} else {
+					"The number 7 was found " + value + " places from the start.";
+				}
+			}
+		}.wait(targetIndex).result;
+		// Wire the function that will trace the message.
 		function(value:String):Void {
 			trace(value);
 		}.wait(message);
 		// Find 7, starting from the start and from the end.
-		var forwardBond:Bond = new BondHolder();
-		var backwardBond:Bond = new BondHolder();
-		function(index:Int):Void {
-			message.yield("The number 7 was found " + index + " places from the start.");
+		var backwardBond:Bond = null;
+		var forwardBond:Bond = function(index:Int):Void {
+			targetIndex.yield(index);
 			// Prevent the backward finding from tracing, too.
 			backwardBond.destroy();
-		}.wait(new ForwardOccurrenceFinder().find(numbers, 7), forwardBond);
-		function(index:Int):Void {
-			message.yield("The number 7 was found " + index + " places from the end.");
+		}.wait(new ForwardOccurrenceFinder().find(numbers, 7));
+		backwardBond = function(index:Int):Void {
+			targetIndex.yield(-index);
 			// Prevent the forward finding from tracing, too.
 			forwardBond.destroy();
-		}.wait(new BackwardOccurrenceFinder().find(numbers, 7), backwardBond);
+		}.wait(new BackwardOccurrenceFinder().find(numbers, 7));
 	}
 }
 class OccurrenceFinder {
@@ -40,7 +55,7 @@ class OccurrenceFinder {
 	public function find(array:Array<Int>, targetNumber:Int):Async<Int> {
 		var result:Async<Int> = new Async(this);
 		var currentIndex:Int = 0;
-		var timer:Timer = new Timer(1);
+		var timer:Timer = new Timer(100);
 		var determineMatch:Array<Int> -> Int -> Int -> Bool = determineMatch;
 		timer.run = function():Void {
 			if (determineMatch(array, targetNumber, currentIndex)) {
